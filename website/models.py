9f3ae8cd4e52779896700c6a8c4e0fd8802c260b
mrxdoffical <berogergesg@gmail.com>
2024-05-31 17:24:31 +0300

    modified:   website/models.py

--- website/models.py
+++ website/models.py
@@ -1,14 +1,12 @@
-from . import db
 from flask_login import UserMixin
 from uuid import uuid4
 from sqlalchemy.sql import func
-
+from . import db
 class Note(db.Model):
     id = db.Column(db.String(36), primary_key=True)
     data = db.Column(db.String(100000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.String, db.ForeignKey('user.id'))
-    
 
 
 class User(db.Model, UserMixin):
@@ -17,3 +15,4 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150), nullable=False)
     firstName = db.Column(db.String(150), nullable=False)
     notes = db.relationship('Note')
+
